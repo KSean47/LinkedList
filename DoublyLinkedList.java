cba3985f6e29de645f24bb02731c4239e8e53e47
KSean47 <skennedy6@albany.edu>
2017-03-26 23:06:45 -0400

    Add files via upload

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -4,34 +4,34 @@ public class DoublyLinkedList
   private int size = 0;
   private Node root = null;
   
-  public void add(int index, Node addedNode) {
+  public void add(int x, Node addedNode) {
     Node newNode = addedNode;
     
-    if( index > this.size)
-      //error
-      System.out.println("Index " + index + " does not exist.");
-    else {
-      // if list is empty, newNode is root
+    if( x > this.size)
+      System.out.println("Index " + x + " does not exist.");
+    else 
+    {
       if (this.root == null) {
         this.root = newNode;
       }
-      // adds to front of list
-      else if (index == 0) {
+      else 
+        if (x == 0) {
         newNode.setNext(root);
         this.root = newNode;
       }
-      else if (index == this.size) {
-        // adds to end of list
+      else 
+        if (x == this.size) 
+      {
         Node current = this.root;
         while (current.getNext() != null) {
           current = current.getNext();
         }
         current.setNext(newNode);
       }
-      else {
-        // adds to middle of list
+      else 
+      {
         Node current = this.root;
-        for(int i = 0; i < index - 1; i++) {
+        for(int i = 0; i < x - 1; i++) {
           current = current.getNext();
         }
         newNode.setNext(current.getNext());
@@ -49,13 +49,15 @@ public class DoublyLinkedList
       Node currentNode = this.root;
       while(currentNode != null)
       {        
-        if(newNode.getName().compareTo(currentNode.getName())==1)
+        if(newNode.getName().compareTo(currentNode.getName())<=0)
         {
-          add(i, newNode);
+          newNode.setId(i);
+          this.add(i, newNode);
         }
         currentNode = currentNode.getNext();
         i++;
       }
+      
   }
   
   public void find(Node findNode)
@@ -72,9 +74,10 @@ public class DoublyLinkedList
         else if(currentNode.getName() == findNode.getName())
         {
           isFound = "Node found.";
+          currentNode = currentNode.getNext();
         }
       }
-      System.out.println(isFound);   
+      System.out.println(isFound);  
   }
   
   public void printList(){
@@ -86,8 +89,40 @@ public class DoublyLinkedList
         System.out.println(currentNode.toString());
         currentNode = currentNode.getNext();
       }
+     
       
   }
+  
+  public void printBackwards()
+  {
     
+  }
+  
+   public void delete(int x) 
+   {
+    Node tmp = this.root;
+    int i = 0;
+    
+    if (x == 0) 
+    {
+      this.root = this.root.getNext();
+    }
+    while (tmp.getNext() != null && i < x-1) 
+    {
+      
+      tmp = tmp.getNext();
+      i++;
+    }
+    if (x < size && x > 0) 
+    {
+      tmp.setNext(tmp.getNext().getNext());
+    size--;
+    }    
+  }
+    
+  public void destroy() 
+  {
+    this.root = null;
+  }
 
 }
